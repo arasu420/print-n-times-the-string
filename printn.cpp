df49bb28c528fc35167e853964d5009919797270
arasu420 <36299277+arasu420@users.noreply.github.com>
2018-03-02 17:25:22 +0530

    Create printn.cpp

---
+++ printn.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <string>
+using namespace std;
+int main()
+{
+    char a[50];
+    int n;
+    cout<<"Enter the string";
+    cin>>a;
+    cout<<"Enter n=";
+    cin>>n;
+    for(int i=0;i<n;i++)
+    {
+        cout<<a<<endl;
+    }
+    return 0;
+}
